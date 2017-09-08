a370b01e64460c03ca290d0adb1daa0ab412ce45
Brian L. Troutwine <blt@postmates.com>
2017-09-07 18:20:48 -0700

    Update main.rs for more vroooom
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 extern crate hopper;
 
-use std::{path, thread};
+use std::{path, thread, time};
 
 fn send(mut sender: hopper::Sender<u16>) -> () {
     let mut cur = 0;
+    let sleep_time = time::Duration::from_millis(1);
     loop {
         sender.send(cur);
         cur = cur.wrapping_add(1);
+        thread::sleep(sleep_time);
     }
 }
 
 fn recv(receiver: hopper::Receiver<u16>) -> () {
-    for _ in receiver {
-        // nothing, intentionally
+    let mut recv = receiver.into_iter();
+    loop {
+        match recv.next() {
+            Some(_) => {
+                // nothing, intentionally
+            },
+            None => {},
+        }
     }
 }
 
@@ -20,7 +28,7 @@ fn main() {
     let mut joins = Vec::new();
     let (sender, receiver) = hopper::channel("fd_test", path::Path::new("/tmp/fd_test")).unwrap();
 
-    for _ in 0..2 {
+    for _ in 0..10 {
         let snd = sender.clone();
         joins.push(thread::spawn(move || send(snd)));
     }
